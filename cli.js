14e629bf47dc90ac8d9cd08debee2321b542492e
Pablo F. Mescher <pfmescher@gmail.com>
2021-09-01 16:06:42 -0300

    Some optimizations

--- cli.js
+++ cli.js
@@ -3,6 +3,7 @@ const { MerkleTree } = require("merkletreejs");
 const SHA256 = require("crypto-js/sha256");
 const fs = require("fs");
 const { Command } = require('commander');
+const readline = require('readline');
 
 const LEAVES_HASH_LEN = 16;
 const DELIMITER = "\t";
@@ -15,15 +16,15 @@ function buildMerkle(content, concatenate=false) {
 
   // read UID and balance from input file
   const total_balances = [];
-  const leaves = [];
+
+  const merkle_output = fs.createWriteStream('output_merkle_tree.txt')
 
   let columns;
   let tokens;
 
-  const list = content.split(/\r?\n/);
 
-  for (let i = 0; i < list.length; i++) {
-    const row = list[i];
+  for (let i = 0; i < content.length; i++) {
+    const row = content[i];
     const data = row.split(",").map(item => item.trim());
 
     // Check if we are in the header row, save the columns and skip
@@ -55,36 +56,38 @@ function buildMerkle(content, concatenate=false) {
       // Remove trailing zeroes using https://stackoverflow.com/a/53397618/1231428
       const sanitized_balances = balances.map(balance => balance.replace(/(\.[0-9]*[1-9])0+$|\.0*$/,"$1"));
       const string_to_hash = `${uid},${tokens.map((token, i) => [token, sanitized_balances[i]].join(":")).join(",")}`;
-      leaves.push(SHA256(string_to_hash));
+      content[i] = SHA256(string_to_hash)
 
     } else {
       const uid_hash = SHA256(uid);
       const concatenated_balances = balances.join('');
       const balance_hash = SHA256(concatenated_balances.toString());
 
-      leaves.push(SHA256(uid_hash.toString() + balance_hash.toString()).toString().substring(0, LEAVES_HASH_LEN)); // underlying data to build Merkle tree
+      content[i] = SHA256(uid_hash.toString() + balance_hash.toString()).toString().substring(0, LEAVES_HASH_LEN); // underlying data to build Merkle tree
     }
   }
 
   // build Merkle tree
-  const tree = new MerkleTree(leaves, SHA256);
+  const tree = new MerkleTree(content, SHA256);
 
   let treeLevels = tree.getLayers().length;
   let leavesFromTree = tree.getLeaves();
-  let output = "Level" + DELIMITER + "Hash" + NEW_LINE;
+
+
+  merkle_output.write("Level" + DELIMITER + "Hash" + NEW_LINE);
   for (let i = 0; i < leavesFromTree.length; i++) {
     // write only the leaf nodes of the Merkle tree into verification file, all letters in lower case
-    output +=
+    merkle_output.write(
       treeLevels +
       "," +
       i.toString() +
       DELIMITER +
       // shorten hashed value in leaves
       leavesFromTree[i].toString("hex") +
-      NEW_LINE;
+      NEW_LINE);
   }
   console.log("Merkle tree complete");
-  return [output, tokens, total_balances, tree];
+  return [merkle_output, tokens, total_balances, tree];
 }
 
 async function main() {
@@ -97,19 +100,31 @@ async function main() {
 
   program.parse();
 
-  const { concatenate, input, newline, delimeter } = program.opts()
+  const { concatenate, input, newline } = program.opts()
 
   if (newline === 'lf') {
     NEW_LINE = '\n'
   }
 
-  try {
-    const content = fs.readFileSync(input, { encoding: 'utf-8' }).toString().trim()
-    const [output, tokens, total_balances, tree] = buildMerkle(content, !!concatenate);
+  const inputStream = fs.createReadStream(input);
+  let content = []
 
-    fs.writeFileSync('output_merkle_tree.txt', output)
+  const rl = readline.createInterface({
+    input: inputStream,
+    crlfDelay: Infinity,
+  });
+
+  for await (let line of rl) {
+    content.push(line);
+  }
+  console.info(content.length);
+  inputStream.close();
+
+  try {
+    const [output, tokens, total_balances, tree] = buildMerkle(content, !!concatenate);
     fs.writeFileSync('output_total_balances.txt', `${tokens.map((token, i) => `${token}:${total_balances[i].toString()}`).join(NEW_LINE)}`)
     fs.writeFileSync('output_merkle_root.txt', tree.getRoot().toString("hex"))
+    output.on("end", () => process.exit(0))
   } catch (e) {
     console.error(e);
     process.exit(1);
